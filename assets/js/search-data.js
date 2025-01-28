977fc390d3f07b4e85642a95ef710ba7fec4f0ed
matteo-misurati <matteo.misurati@gmail.com>
2025-01-28 12:13:47 +0000

    Deploying to gh-pages from @ matteo-misurati/matteo-misurati.github.io@f8a90675bf046ef3878b1f39b434a046f7627495 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-talks",
           title: "talks",
           description: "talks at conferences, with slides! yahoooooooo",
           section: "Navigation",
           handler: () => {
             window.location.href = "/talks/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
